a653c4842d926874313c2d0d50f84c3e99ea0704
Sangeetha Sambantham <sangeethaisbetterthanwarmjeansfromthedryer@helpusdefend.com>
2025-01-29 20:59:00 -0600

    Admin update missing usernames

--- vbb/users/admin.py
+++ vbb/users/admin.py
@@ -29,16 +29,23 @@ class UserAdmin(auth_admin.UserAdmin):
     list_display = ["username", "email", "first_name", "last_name", "is_superuser"]
     search_fields = ["first_name", "last_name"]
 
-
-
-
     # Custom admin action
     @admin.action(description="Update missing usernames")
     def update_missing_usernames(self, request, queryset):
         for user in queryset:
-            if not user.username:  # Check if the username is missing
-                user.username = f"{user.first_name}{user.last_name}".replace(" ", "")
+            if not user.username:  # Check if username is missing
+                base_username = f"{user.first_name}{user.last_name}".replace(" ", "")
+                new_username = base_username
+                counter = 1
+
+                # Ensure username is unique
+                while User.objects.filter(username=new_username).exists():
+                    new_username = f"{base_username}{counter}"
+                    counter += 1
+
+                user.username = new_username
                 user.save()
+
         self.message_user(request, "Usernames updated successfully.")
 
     # Register the custom action
